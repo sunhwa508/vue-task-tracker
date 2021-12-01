433aaf4bbc5e727633c3fd4402461de452c39a30
beatcorp mac02 <>
2021-12-01 14:40:45 +0900

    ✨ [feat] add task tracker components

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+      @btn-click="$emit('toggle-add-task')"
+    />
+  </header>
+</template>
+<script lang="tsx">
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Task Traker",
+    },
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+
+  emits: ["toggle-add-task"],
+};
+</script>
+// scoped 붙이면 여기만 적용!
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
